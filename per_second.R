60a4a25f44864c76a04876cea4866b454aef313a
Tim Dresser <tdresser@chromium.org>
2017-09-15 15:02:23 -0400

    Fix issue with NAs for per_second

--- per_second.R
+++ per_second.R
@@ -34,14 +34,17 @@ per_second_organized$cache_temperature <- factor(per_second_organized$cache_temp
 levels(per_second_organized$is_cpu_time) <- c("Wall Clock Time", "CPU Time")
 
 per_second_breakdowns_together <- per_second_organized %>% 
-  filter(start < 60) %>% 
+  filter(start < 30) %>% 
   group_by(site, cache_temperature, start, end, is_cpu_time, breakdown) %>% 
-  dplyr::summarize(value=mean(value))  %>% # Where are the duplicates coming from?
+  dplyr::summarize(value=value)  %>% # Where are the duplicates coming from?
   spread(breakdown, value)
   
+# Mean fails when NAs are removed.
+per_second_breakdowns_together[is.na(per_second_breakdowns_together)] <- 0
+
 per_second <- per_second_breakdowns_together %>%
   group_by(start, end, cache_temperature, is_cpu_time) %>%
-  dplyr::summarise_at(vars(-site), funs(mean(., na.rm=TRUE))) %>%
+  dplyr::summarise_at(vars(-site), funs(mean(.))) %>%
   gather(breakdown, value, -start, -cache_temperature, -end, -is_cpu_time) %>%
   filter(breakdown != 'total')
 
@@ -50,4 +53,4 @@ plot_important_times <- per_second %>% ggplot(aes(x=start, y=value, fill=breakdo
   scale_fill_manual(values=breakdown_colors) +
   facet_grid(~cache_temperature~is_cpu_time) +
   labs(title="Mean Contributors over time.", x="Time in Seconds", y="Seconds")
-ggplotly(plot_important_times)
+ggplotly(plot_important_times, tooltip="text")
